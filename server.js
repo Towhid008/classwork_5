3040bf11e4c66b0caff81a7e8008f84f7e533be1
Md Towhidul Islam <1310889@iub.edu.bd>
2018-07-30 04:35:20 +0000

    classwork-9

--- server.js
+++ server.js
@@ -6,11 +6,28 @@ var http = require('http');
   app.use(bodyParser.urlencoded({extended:true}));
   var server = http.Server(app);
   var mongo = require('mongodb');
+
+  
   //for local
   //var db, uri = "mongodb://localhost:27817";
   //for c9
   var db, uri = "mongodb://"+process.env.IP+":27017";
+  const mongoose = require('mongoose');
+   mongoose.connect ("mongodb://localhost:27017/node-cw8");
   
+  mongoose.connection.on ('error', function(){
+    console.log('Could not connect to mongodb');
+  });
+  
+  var userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: "Name is required"
+    },
+    email: String
+  });
+  var User= mongoose.model('User', userSchema);
+  /*
   mongo.MongoClient.connect(uri,
           {useNewUrlParser:true},
           function(err, client){
@@ -26,18 +43,26 @@ var http = require('http');
     var collection = db.collection('users');
     collection.save(form_data);
   }
-  
+  */
   app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
   });
   
   app.post('/submit_user', function(req, res){
     console.log(req.body);
-    save(req.body);
-    res.status(200);
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if(err){
+        return res.status(400)
+        .json({message:"Could not save user"})
+      }
+      res.status(200).json(data);
+    })
+   // save(req.body);
+    //res.status(200);
   });
   
-  app.get('/about', function(req, res){
+  app.get('/system/about', function(req, res){
     res.sendFile(__dirname+'/about.html');
   });
   
